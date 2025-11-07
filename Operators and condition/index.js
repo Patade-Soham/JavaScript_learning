1fc29bc0fda9a70b62cfc4ea8bf02a928292e60e
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-07 07:32:40 +0530

    operators and conditons

---
+++ Operators and condition/index.js
@@ -0,0 +1,20 @@
+let x = 20;
+let y =10;
+let z=x-y;
+
+console.log(z);
+
+y=z**x;
+console.log(y);
+
+
+
+let age =18;
+let message="you can not drive";
+if(age>=18){
+    message="you can drive";
+    console.log(message);
+}
+else{
+    console.log(message);
+}
