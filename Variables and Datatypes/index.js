597f7dde68defc5cf359cfddc84bd15fb8afb401
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-07 06:47:52 +0530

    var and datatypes

---
+++ Variables and Datatypes/index.js
@@ -0,0 +1,46 @@
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color, "and ",lastName);
+
+// Number
+let length = 16;
+let weight = 7.5;
+console.log(length,"and ",weight);
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345);
+console.log(x,"and ",y);
+
+// Boolean
+let a = true;
+let b = false;
+console.log(a,"and ",b);
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person);
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+// Date object
+const date = new Date("2022-03-25");
+console.log(date);
+// Undefined
+let c;
+let d;
+console.log(c,"and ",d);
+
+// Null
+let e = null;
+let f = null;
+console.log(e,"and ",f);
+// Symbol
+const g = Symbol();
+const h = Symbol();
+console.log(g,"and ",h);
+
+x=10;
+y=40;
+console.log(x,"and ",y)
