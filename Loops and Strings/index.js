3ceaf01671a8f766c7fbcf777ebf2fba98f0e457
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-07 08:55:25 +0530

    loops and strings

---
+++ Loops and Strings/index.js
@@ -0,0 +1,30 @@
+let i = 5;
+
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+i=3;
+while(i<10){
+    console.log(i);
+    i = i+1;
+}
+
+
+
+
+let firstName = "John";
+let lastName = "Doe";
+
+let text = `Welcome ${firstName}, ${lastName}!`;
+console.log(text);
+
+
+let s1="som"
+let s2="Ar"
+let s3=s1+s2;
+console.log(s3);
+
+console.log(s1.length);
+console.log(s1.toLocaleUpperCase);
+console.log(s1.charAt[2]);
+console.log(s1.substring(1,2));
