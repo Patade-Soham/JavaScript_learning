5b2bf2ac76dab1e8aa8673ef83c7b0b5e28b6ae0
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-11 23:28:40 +0530

    streak saved sucessfully

---
+++ JavascriptsFuntasks/practice_set4.js
@@ -0,0 +1,19 @@
+//Q1 What will the following print in Js
+console.log("som\"".length);
+// som"
+
+//Q2 Explore the includes, start with and ends with functions of a string
+
+//Q3 WAP to convert a given string to lowercase
+let st = "SOHAM"
+console.log(st.toLowerCase);
+
+//Q4 Extract the amount out of this string "Please give Rs 1000"
+
+let exstr="Please give Rs 1000";
+
+let amount= exstr.slice("Please give Rs".length);
+console.log(amount);
+
+//Q5 Try to Change 4th Character of a given string were you able to do it?
+//NO
