14582b6b3b3d81d04707ab1a80be7d12a2a60db9
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-12 23:28:15 +0530

    Arrays intro

---
+++ Arrays/index.js
@@ -0,0 +1,28 @@
+const list = [23,43,14,"str",false];
+console.log(list.length);
+console.log(list[3]);
+console.log(list[3][0]);
+
+const nums=[13,445,56,78,45,48]
+
+nums.sort((a, b) => a - b);
+console.log(nums); 
+
+nums.push(14);
+console.log(nums)
+
+
+nums[6]=57
+console.log(nums)
+
+nums.pop(nums[4])
+console.log(nums)
+
+
+
+
+
+
+
+
+
