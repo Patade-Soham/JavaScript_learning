c3f52baa3b2a4cff66903d8ee0828513feef394e
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-13 16:17:41 +0530

    arrays

--- Arrays/index.js
+++ Arrays/index.js
@@ -28,6 +28,44 @@ for(let number in nums){
 for(let numbers of nums){
     console.log(numbers)
 }
+// More Methods in arrays 
+
+const arr=[1,4,5,6,8]
+console.log(arr.toString())
+
+console.log(arr.join("-"))
+
+const newarr=[1,4,5,6,8]
+newarr.pop()
+
+console.log(newarr)
+
+newarr.push(8)
+console.log(newarr)
+
+newarr.shift()
+console.log(newarr)
+
+newarr.unshift(3)
+console.log(newarr)
+
+delete newarr[2]
+console.log(newarr)
+
+console.log(newarr.concat(arr))
+
+newarr.splice(2,2,77,88,55,66)
+console.log(newarr)
+
+console.log(newarr.slice(1,3))
+console.log(newarr.slice(2))
+
+
+let b=newarr.map((value)=>{
+    console.log(value)
+    return value+1
+})
+console.log(b);
 
 
 
