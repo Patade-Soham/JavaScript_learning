b7dec2254af197434744bc49b6ff5df7c6997d29
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-16 22:36:00 +0530

    console methods in js

---
+++ console methods/script.js
@@ -0,0 +1,8 @@
+console.log('hello')
+console.log(console) //to print all console methods on the console page
+
+console.assert("this is assert")
+console.info('info')
+console.warn('warn')
+console.error('error')
+alert('boom!')
