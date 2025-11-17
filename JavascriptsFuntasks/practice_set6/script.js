870f8cfab34759e1d6a1438303ff1fa1d953027e
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-11-17 23:21:00 +0530

    Js in browser is cool

---
+++ JavascriptsFuntasks/practice_set6/script.js
@@ -0,0 +1,29 @@
+//Q1 write a programe using prompt function to take input of age as a value from the user 
+// and use alert to tell him if he can drive or not 
+
+//Q2 in q1 use confirm to ask the user if he wants to see the prompt again
+//Q3 In the previous questionuse console.error to log the error if age is negetive
+
+// let runAgain=true;
+// while(runAgain){
+//     let inp = parseInt(prompt("What is your age:"));
+
+//     if (inp < 18) {
+//         alert("You cannot drive, broski!");
+//     } else {
+//         alert("You can drive, Yeah!");
+//     }
+//     runAgain=confim('Do you want to go again ?');
+
+// }
+
+//change the background of the page to yellow red or any other color based on user input through prompt
+
+// let color=prompt('Enter a color');
+// document.body.style.background=color;
+
+//Write a program to change the url to google.com if user enters a number greater than 4 
+let redir=Number.parseInt(prompt('enter a number'))
+if(redir>4){
+    location.href="http://google.com"
+}
