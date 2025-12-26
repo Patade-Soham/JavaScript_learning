e938a35dc60ba0ac80c238ec54f81819c8c0c34f
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-12-27 02:18:48 +0530

    took break coz of health but full power tommorow

--- Events and other DoM properties/script.js
+++ Events and other DoM properties/script.js
@@ -19,10 +19,32 @@
 // console.log(first.getAttribute('name'));
 // console.log(first.dataset.cat)
 
-let a = document.getElementsByTagName('div')[0]
-a.innerHTML = a.innerHTML + '<h1>scratch scratch, Astroid destroyer is here </h1>'
+// let a = document.getElementsByTagName('div')[0]
+// a.innerHTML = a.innerHTML + '<h1>scratch scratch, Astroid destroyer is here </h1>'
 
-let div = document.createElement('div');
-div.innerHTML='<h1>Galaxy paww !</h1>'
+// let div = document.createElement('div');
+// div.innerHTML='<h1>Galaxy paww !</h1>'
 
-a.append(div);
+// a.append(div);
+
+let a = document.getElementById('first')
+// a.insertAdjacentHTML('beforebegin','<h1> BB </h1>')
+// a.insertAdjacentHTML('beforeend','<h1> Be </h1>')
+// a.insertAdjacentHTML('afterbegin','<h1> ab </h1>')
+// a.insertAdjacentHTML('afterend','<h1> ae </h1>')
+
+// a.className= 'text-black red'
+// a.classList.remove('red')
+// a.classList.add('yellow')
+
+// let b = setTimeout(function(){
+//     alert('i am set timeout')
+// },5000);
+
+// console.log(b)
+
+// const sum =(a,b)=>{
+//     console.log(a+b)
+// }
+
+// setTimeout(sum,2000,60,7)
