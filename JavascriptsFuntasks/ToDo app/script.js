aee43e5308b68c6a85205ef8e963ccb24e0d70d4
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2026-01-20 03:06:15 +0530

    done with add and figured out the fault

--- JavascriptsFuntasks/ToDo app/script.js
+++ JavascriptsFuntasks/ToDo app/script.js
@@ -1,29 +1,44 @@
-let add_button = document.getElementById('form_button')
+const add_button = document.getElementById('form_button')
+const del_button = document.getElementById('delete')
+const comp_button = document.getElementById('Completed')
+let i = 0;
 
-let add_task = (e)=>{
-    e.preventDefault()
-    const task = document.getElementById('inp').value
-
-    let taskbox = 
+const add =function addTask(){
+    
+    let task = document.getElementById('inp').value;
+    let taskstr = 
     `
                 <div class="task">
                     <div class="text">
                         <h4>${task}</h4>
                     </div>
+                    <button id="Completed">Completed</button>
                     <button id="delete">delete</button>
                 </div>
-    `
-    document.getElementById('taskbox').innerHTML= taskbox
+    
+    
+    `;
+    if(task.trim()==''){
+        alert('Enter a task to add')
+    }else{
+        let tasknode=document.createElement('div');
+        tasknode.className='task'
+        tasknode.id = `taskName${i}`
+        tasknode.innerHTML=taskstr;
+        document.getElementById('mainbox').append(tasknode)
+        localStorage.setItem(`taskName${i}`,task)
+        i++;
+        // task should remain even after storing
+    }
+
+}
+const done =function update(){
+    alert('done') // these task should be done on event basis i.e. where the event is happening
+}
+const del =function addTask(){
+    alert('deleted')
 }
 
-
-
-add_button.addEventListener('click',add_task)
-let del_button = document.getElementById('delete')
-
-let del_task = (e)=>{
-    document.getElementById('taskbox').innerHTML= ''
-    e.preventDefault()
-}
-
-del_button.addEventListener('click',del_task)
+add_button.addEventListener('click',add)
+del_button.addEventListener('click',del)
+comp_button.addEventListener('click',done)// wont work cause buttons are been created after adding items needs to be fixed
