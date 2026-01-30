812a00d398a4df4ff083f4c2708d309799ac781b
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2026-01-30 22:42:58 +0530

    today level 2 will be cleared

--- Javascript Mastery/L2/script.js
+++ Javascript Mastery/L2/script.js
@@ -1,16 +1,31 @@
-let head = document.getElementById('page-heading');
-let p1 = document.querySelector('p');
-let p2 = document.body.getElementsByClassName('box')
-let btn=document.getElementById('btn')
+// let head = document.getElementById('page-heading');
+// let p1 = document.querySelector('p');
+// let p2 = document.body.getElementsByClassName('box')
+// let btn=document.getElementById('btn')
 
-// btn.style.background='yellow';
-// p1.style.background='green'
-// head.style.background='cyan'
-let counter =0
-btn.addEventListener('click',()=>{
-    counter +=1
-    p1.innerHTML = 
-    `
-    ${counter}
-    `
+// // btn.style.background='yellow';
+// // p1.style.background='green'
+// // head.style.background='cyan'
+// let counter =0
+// btn.addEventListener('click',()=>{
+//     counter +=1
+//     p1.innerHTML = 
+//     `
+//     ${counter}
+//     `
+// })
+
+// const input = document.getElementById('userInput');
+// const preview = document.getElementById('livePreview');
+// // Listen for real-time input
+// input.addEventListener('input', () => {
+//   preview.innerText = input.value; // Updates the paragraph
+// })
+
+let subBtn = document.getElementById('sub')
+let inpBtn = document.getElementById('inp')
+
+subBtn.addEventListener('click',(e)=>{
+  document.body.innerHTML=`<p>${inpBtn.value}</p>`
+  preventDefault(e);
 })
