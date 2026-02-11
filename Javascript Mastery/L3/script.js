225f91190dc6367f4d012f566db3bcef6a7fc709
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2026-02-11 23:41:48 +0530

    back and I will end Js this time fs

---
+++ Javascript Mastery/L3/script.js
@@ -0,0 +1,52 @@
+const quizData = [
+    {
+        question: "Which language is used to style web pages?",
+        options: ["HTML", "JQuery", "CSS", "XML"],
+        answer: "CSS"
+    },
+    {
+        question: "Which language is used for web app logic?",
+        options: ["Python", "JavaScript", "C++", "Java"],
+        answer: "JavaScript"
+    },
+    {
+        question: "Inside which HTML tag do we put JavaScript?",
+        options: ["<js>", "<script>", "<javascript>", "<code>"],
+        answer: "<script>"
+    },
+    {
+        question: "Which company developed JavaScript?",
+        options: ["Microsoft", "Sun Microsystems", "Netscape", "Google"],
+        answer: "Netscape"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "Computer Style Sheets",
+            "Creative Style System",
+            "Cascading Style Sheets",
+            "Colorful Style Sheets"
+        ],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Which method is used to select an element by ID in JavaScript?",
+        options: [
+            "getElementByClass()",
+            "getElementById()",
+            "querySelectorAll()",
+            "getElementsByTagName()"
+        ],
+        answer: "getElementById()"
+    },
+    {
+        question: "Which symbol is used for comments in JavaScript?",
+        options: ["//", "/* */", "#", "<!-- -->"],
+        answer: "//"
+    },
+    {
+        question: "Which HTML attribute is used to define inline styles?",
+        options: ["class", "font", "styles", "style"],
+        answer: "style"
+    }
+];
